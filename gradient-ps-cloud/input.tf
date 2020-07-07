e6437801c6b72e670833c8b56221c445a4feaba0
paperspace-philip <58186049+paperspace-philip@users.noreply.github.com>
2020-07-07 18:16:08 -0400

    feat(ps-cloud): managed ps-cloud install with ansible, published as a container (#58)
    Co-authored-by: Jared Scheib <jared.scheib@gmail.com>

--- gradient-ps-cloud/input.tf
+++ gradient-ps-cloud/input.tf
@@ -1,53 +1,109 @@
-variable "admin_email" {}
+variable "admin_email" {
+    description = "Paperspace admin API email"
+}
 
-variable "admin_user_api_key" {}
+variable "admin_user_api_key" {
+    description = "Paperspace admin API key"
+}
 
-# variable "cluster_id" {}
+variable "api_host" {
+    description = "api host"
+    default = "api.paperspace.io"
+}
+
+variable "aws_access_key_id" {
+    description = "AWS access key id"
+    default = ""
+}
+variable "aws_secret_access_key" {
+    description = "AWS secret access key"
+    default = ""
+}
+
+variable "cloudflare_api_key" {
+    description = "Cloudflare API key"
+    default = ""
+}
+variable "cloudflare_email" {
+    description = "Cloudflare email"
+    default = ""
+}
+variable "cloudflare_zone_id" {
+    description = "Cloudflare zone id"
+    default = ""
+}
+
+variable "cluster_id_integer" {
+    description = "Cluster id integer"
+}
+
+variable "is_proxied" {
+    type = bool
+    description = "Is PS Cloud cluster managed by Paperspace"
+    default = false
+}
 
 variable "machine_storage_main" {
-    default = 50
+    type = number
+    description = "Main storage id"
+    default = 100
 }
 variable "machine_template_id_main" {
-    default = "t04azgph"
+    description = "Main template id"
+    default = "tmun4o2g" # tmun4o2g is pre-installed with nvidia and docker; docker is needed for cpu, whereas nvidia is needed for gpu, but using this template introduces very little bloat and speeds up node configuration
 }
 variable "machine_type_main" {
+    description = "Main machine type"
     default = "C5"
 }
 
 variable "machine_count_worker_cpu" {
+    type = number
+    description = "Number of CPU workers"
     default = 3
 }
 variable "machine_storage_worker_cpu" {
-    default = 50
+    type = number
+    description = "CPU worker storage"
+    default = 100
 }
 variable "machine_template_id_cpu" {
-    default = "t04azgph"
+    description = "CPU template id"
+    default = "tmun4o2g" # tmun4o2g is pre-installed with nvidia and docker; docker is needed for cpu, whereas nvidia is needed for gpu, but using this template introduces very little bloat and speeds up node configuration
 }
 variable "machine_type_worker_cpu" {
+    description = "CPU worker machine type"
     default = "C5"
 }
 
 variable "machine_count_worker_gpu" {
+    type = number
+    description = "Number of GPU workers"
     default = 3
 }
 variable "machine_storage_worker_gpu" {
-    default = 50
+    type = number
+    description = "GPU worker storage"
+    default = 100
 }
 variable "machine_template_id_gpu" {
+    description = "GPU template id"
     default = "tmun4o2g"
 }
 variable "machine_type_worker_gpu" {
+    description = "GPU worker machine type"
     default = "P4000"
 }
 
-variable "network_id" {}
-
 variable "region" {
+    description = "Cloud region"
     default = "East Coast (NY2)"
 }
 
-variable "ssh_key_private" {}
+variable "team_id" {
+    description = "Cluster team id"
+}
 
-variable "ssh_key_public" {}
-
-variable "team_id" {}
+variable "team_id_integer" {
+    description = "Cluster team id integer"
+}
--- gradient-ps-cloud/main.tf
+++ gradient-ps-cloud/main.tf
@@ -1,3 +1,11 @@
+locals {
+    ssh_key_path = "${path.module}/ssh_key"
+}
+
+resource "tls_private_key" "ssh_key" {
+    algorithm = "RSA"
+}
+
 provider "paperspace" {
     region = var.region
     api_key = var.admin_user_api_key
@@ -8,25 +16,36 @@ data "paperspace_user" "admin" {
     team_id = var.team_id
 }
 
-data "paperspace_network" "network" {
-    id = var.network_id
+resource "paperspace_script" "add_public_ssh_key" {
+    name = "Add public SSH key"
+    description = "Add public SSH key on machine create"
+    script_text = <<EOF
+        #!/bin/bash
+        echo "${tls_private_key.ssh_key.public_key_openssh}" >> /home/paperspace/.ssh/authorized_keys
+    EOF
+    is_enabled = true
+    run_once = true
+
+    provisioner "local-exec" {
+        command = <<EOF
+            sleep 20
+        EOF
+    }
 }
 
-resource "paperspace_script" "add_public_ssh_key" {
-  name = "Add public SSH key"
-  description = "Add public SSH key on machine create"
-  script_text = <<EOF
-#!/bin/bash
-echo "${var.ssh_key_public}" >> /home/paperspace/.ssh/authorized_keys
-EOF
-  is_enabled = true
-  run_once = true
+resource "paperspace_network" "network" {
+    team_id = var.team_id_integer
 }
 
 resource "paperspace_machine" "gradient_main" {
+    depends_on = [
+        paperspace_script.add_public_ssh_key,
+        tls_private_key.ssh_key,
+    ]
+
     region = var.region
-    name = "${var.name}-main"
-    machine_type = var.machine_type_main
+    name = "${var.cluster_handle}-${var.name}-main"
+    machine_type = "P4000"
     size = var.machine_storage_main
     billing_type = "hourly"
     assign_public_ip = true
@@ -34,24 +53,41 @@ resource "paperspace_machine" "gradient_main" {
     user_id = data.paperspace_user.admin.id
     team_id = data.paperspace_user.admin.team_id
     script_id = paperspace_script.add_public_ssh_key.id
-    # cluster_id = var.cluster_id // coming soon
+    network_id = paperspace_network.network.handle
+    live_forever = true
+
+    provisioner "remote-exec" {
+        connection {
+            type     = "ssh"
+            user     = "paperspace"
+            host     = self.public_ip_address
+            private_key = tls_private_key.ssh_key.private_key_pem
+        }
+    } 
 
     provisioner "local-exec" {
         command = <<EOF
+            echo "${tls_private_key.ssh_key.private_key_pem}" > ${local.ssh_key_path} && chmod 600 ${local.ssh_key_path} && \
             ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+            --key-file ${local.ssh_key_path} \
             -i '${paperspace_machine.gradient_main.public_ip_address},' \
             -e "install_nfs_server=true" \
-            -e "nfs_subnet_host_with_netmask=${data.paperspace_network.network.network}/${data.paperspace_network.network.netmask}" \
-            ansible/playbook-gradient-metal-ps-cloud-node.yaml
+            -e "nfs_subnet_host_with_netmask=${paperspace_network.network.network}/${paperspace_network.network.netmask}" \
+            ${path.module}/ansible/playbook-gradient-metal-ps-cloud-node.yaml
         EOF
     }
 }
 
 resource "paperspace_machine" "gradient_workers_cpu" {
+    depends_on = [
+        paperspace_script.add_public_ssh_key,
+        tls_private_key.ssh_key,
+    ]
+
     count = var.machine_count_worker_cpu
     region = var.region
-    name = "${var.name}-worker-cpu-${count.index}"
-    machine_type = var.machine_type_worker_cpu
+    name = "${var.cluster_handle}-${var.name}-worker-cpu-${count.index}"
+    machine_type = "P4000"
     size = var.machine_storage_worker_cpu
     billing_type = "hourly"
     assign_public_ip = true
@@ -59,21 +95,38 @@ resource "paperspace_machine" "gradient_workers_cpu" {
     user_id = data.paperspace_user.admin.id
     team_id = data.paperspace_user.admin.team_id
     script_id = paperspace_script.add_public_ssh_key.id
-    # cluster_id = var.cluster_id
+    network_id = paperspace_network.network.handle
+    live_forever = true
+
+    provisioner "remote-exec" {
+        connection {
+            type     = "ssh"
+            user     = "paperspace"
+            host     = self.public_ip_address
+            private_key = tls_private_key.ssh_key.private_key_pem
+        }
+    } 
 
     provisioner "local-exec" {
         command = <<EOF
+            echo "${tls_private_key.ssh_key.private_key_pem}" > ${local.ssh_key_path} && chmod 600 ${local.ssh_key_path} && \
             ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+            --key-file ${local.ssh_key_path} \
             -i '${self.public_ip_address},' \
-            ansible/playbook-gradient-metal-ps-cloud-node.yaml
+            ${path.module}/ansible/playbook-gradient-metal-ps-cloud-node.yaml
         EOF
     }
 }
 
 resource "paperspace_machine" "gradient_workers_gpu" {
+    depends_on = [
+        paperspace_script.add_public_ssh_key,
+        tls_private_key.ssh_key,
+    ]
+
     count = var.machine_count_worker_gpu
     region = var.region
-    name = "${var.name}-worker-gpu-${count.index}"
+    name = "${var.cluster_handle}-${var.name}-worker-gpu-${count.index}"
     machine_type = var.machine_type_worker_gpu
     size = var.machine_storage_worker_gpu
     billing_type = "hourly"
@@ -82,13 +135,25 @@ resource "paperspace_machine" "gradient_workers_gpu" {
     user_id = data.paperspace_user.admin.id
     team_id = data.paperspace_user.admin.team_id
     script_id = paperspace_script.add_public_ssh_key.id
-    # cluster_id = var.cluster_id
+    network_id = paperspace_network.network.handle
+    live_forever = true
+
+    provisioner "remote-exec" {
+        connection {
+            type     = "ssh"
+            user     = "paperspace"
+            host     = self.public_ip_address
+            private_key = tls_private_key.ssh_key.private_key_pem
+        }
+    } 
 
     provisioner "local-exec" {
         command = <<EOF
+            echo "${tls_private_key.ssh_key.private_key_pem}" > ${local.ssh_key_path} && chmod 600 ${local.ssh_key_path} && \
             ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+            --key-file ${local.ssh_key_path} \
             -i '${self.public_ip_address},' \
-            ansible/playbook-gradient-metal-ps-cloud-node.yaml
+            ${path.module}/ansible/playbook-gradient-metal-ps-cloud-node.yaml
         EOF
     }
 }
@@ -114,7 +179,7 @@ module "gradient_metal" {
     elastic_search_host = var.elastic_search_host
     elastic_search_index = var.name
     elastic_search_password = var.elastic_search_password
-    elastic_search_user = "elastic"
+    elastic_search_user = var.elastic_search_user
 
     helm_repo_password = var.helm_repo_password
     helm_repo_username = var.helm_repo_username
@@ -153,10 +218,88 @@ module "gradient_metal" {
 
     shared_storage_path = "/srv/gradient"
     shared_storage_server = paperspace_machine.gradient_main.private_ip_address
-    ssh_key = var.ssh_key_private
+    ssh_key = tls_private_key.ssh_key.private_key_pem
     ssh_user = "paperspace"
 }
 
-output "main_publicIpAddress" {
+resource "null_resource" "complete_cluster_create" {
+    depends_on = [module.gradient_metal]
+
+    provisioner "local-exec" {
+        command = <<EOF
+            curl -H 'Content-Type:application/json' -H 'X-API-Key: ${var.cluster_apikey}' -XPOST '${var.api_host}/clusters/updateCluster' -d '{"id":"${var.cluster_handle}", "attributes":{"networkId":"${paperspace_network.network.id}"}}'
+        EOF
+    }
+}
+
+
+// Disable because of metal for now
+/*
+resource "null_resource" "add_machine_to_cluster_main" {
+    depends_on = [module.gradient_metal]
+
+    provisioner "local-exec" {
+        command = <<EOF
+            curl -H 'Content-Type:application/json' -H 'X-API-Key: ${var.cluster_apikey}' -XPOST '${var.api_host}/clusterMachines/register' -d '{"clusterId":"${var.cluster_handle}", "machineId":"${paperspace_machine.gradient_main.id}"}'
+        EOF
+    }
+}
+
+resource "null_resource" "add_machine_to_cluster_worker_cpu" {
+    depends_on = [module.gradient_metal]
+
+    count = var.machine_count_worker_cpu
+
+    provisioner "local-exec" {
+        command = <<EOF
+            curl -H 'Content-Type:application/json' -H 'X-API-Key: ${var.cluster_apikey}' -XPOST '${var.api_host}/clusterMachines/register' -d '{"clusterId":"${var.cluster_handle}", "machineId":"${paperspace_machine.gradient_workers_cpu[count.index].id}"}'
+        EOF
+    }
+}
+
+resource "null_resource" "add_machine_to_cluster_worker_gpu" {
+    depends_on = [module.gradient_metal]
+
+    count = var.machine_count_worker_gpu
+
+    provisioner "local-exec" {
+        command = <<EOF
+            curl -H 'Content-Type:application/json' -H 'X-API-Key: ${var.cluster_apikey}' -XPOST '${var.api_host}/clusterMachines/register' -d '{"clusterId":"${var.cluster_handle}", "machineId":"${paperspace_machine.gradient_workers_gpu[count.index].id}"}'
+        EOF
+    }
+}
+*/
+
+provider "cloudflare" {
+    version = "~> 2.0"
+    email   = var.cloudflare_email
+    api_key = var.cloudflare_api_key
+}
+
+resource "cloudflare_record" "subdomain" {
+    count = var.cloudflare_api_key == "" && var.cloudflare_email == "" && var.cloudflare_zone_id == "" ? 0 : 1
+    zone_id = var.cloudflare_zone_id
+    name    = var.domain
+    value   = paperspace_machine.gradient_main.public_ip_address
+    type    = "A"
+    ttl     = 3600
+    proxied = var.is_proxied
+}
+
+resource "cloudflare_record" "subdomain_wildcard" {
+    count = var.cloudflare_api_key == "" && var.cloudflare_email == "" && var.cloudflare_zone_id == "" ? 0 : 1
+    zone_id = var.cloudflare_zone_id
+    name    = "*.${var.domain}"
+    value   = paperspace_machine.gradient_main.public_ip_address
+    type    = "A"
+    ttl     = 3600
+    proxied = var.is_proxied
+}
+
+output "main_node_public_ip_address" {
   value = paperspace_machine.gradient_main.public_ip_address
+}
+
+output "network_handle" {
+    value = paperspace_network.network.handle
 }
